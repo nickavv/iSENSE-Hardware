b9aee9af8f0e863c2386b62773ba6b6edd486ebf
Nicholas Ver Voort <moldorma@gmail.com>
2014-03-28 10:51:30 -0400

    Added automated test for the API that can actually be run by the script which Travis calls! Currently tests nothing but it successfully executes

---
+++ Android/iSENSE Imports/src/edu/uml/cs/isense/test/APITest.java
@@ -0,0 +1,28 @@
+package edu.uml.cs.isense.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for API.java.
+ *
+ * @author moldorma@gmail.com (Nick Ver Voort)
+ */
+@RunWith(JUnit4.class)
+public class APITest {
+
+    @Test
+    public void thisAlwaysPasses() {
+    	
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    	
+    }
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 echo Starting test...
-cd Android/iSENSE\ Imports/src/edu/uml/cs/isense/comm/
+cd ./Android/iSENSE\ Imports/
+java -cp ./bin/isense\ imports.jar:./lib/junit-4.10.jar \
+org.junit.runner.JUnitCore edu.uml.cs.isense.test.APITest
 echo Test complete!
